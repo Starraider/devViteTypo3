23749febfa6498afd1f1c59f66a19874222afd2a
Sven Kalbhenn <sven@skom.de>
2023-11-25 23:13:52 +0800

    [TASK](CD): Config deplo.php

--- deploy.php
+++ deploy.php
@@ -11,7 +11,7 @@ require 'recipe/composer.php';
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/vendor/mittwald/deployer-recipes/recipes/deploy.php';
 
-set('repository', 'https://github.com/...');
+set('repository', 'https://github.com/Starraider/devViteTypo3.git');
 
 mittwald_app('p-pey4f8')
     ->set('public_path', '/public');
