6e14c588a8753ade15754f6e345bfdb26f7ad7d4
Sven Kalbhenn <sven@skom.de>
2024-03-14 19:43:24 +0700

    [TASK]: Add vite assets

--- packages/vite_base_sitepackage/ext_localconf.php
+++ packages/vite_base_sitepackage/ext_localconf.php
@@ -4,7 +4,7 @@ defined('TYPO3') or die('Access denied.');
  * Add default RTE configuration
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['vite_base_sitepackage'] = 'EXT:vite_base_sitepackage/Configuration/RTE/Default.yaml';
-
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['vite_asset_collector']['defaultManifest'] = 'EXT:vite_base_sitepackage/Resources/Public/Vite/manifest.json';
 /***************
  * PageTS
  */
